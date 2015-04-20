dab6961842d2f84ee614412c8dec059c148d1b74
Константин <kostya_dedov@mail.ru>
2015-04-20 23:43:27 +0600

    Initialize zero for BigInteger

---
+++ Huffman/BigInteger/initiliase.c
@@ -0,0 +1,11 @@
+#include "../HEADER/HeaderHuffman.h"
+
+//Создание нуля для BigInteger
+
+BigInteger initiliase(void){
+	BigInteger result;
+
+	result.first = result.second = result.third = result.fourth = 0;
+
+	return result;
+}

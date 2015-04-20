f23920ccc369225d10505c98795735bc04973600
Константин <kostya_dedov@mail.ru>
2015-04-20 21:04:07 +0600

    Edit text in stdout from stdin.

---
+++ Archiver/Help-functions/Edit-text/editText.c
@@ -0,0 +1,46 @@
+#include "../../HEADER/HeaderArchiver.h"
+
+/*********************************************************************
+Редактирование текста в консоли.
+s - редактируемая строка (не более чем из MAX_PATH символов)
+*********************************************************************/
+
+
+void editText(char **s)
+{
+	__int64 pos = strlen(*s);
+	unsigned char c;
+
+	setlocale(LC_ALL, "English");
+
+	putchar('\r');
+	printTextWithCursor(*s, pos);	
+	while((c = getch())!= BUTTON_ENTER)
+	{
+		putchar('\r');
+		switch(c)
+		{
+		case BUTTON_ARROW_OR_DELETE:
+			c = getch();
+			if (c == BUTTON_LEFT && pos > 0)
+				pos--;
+			if (c == BUTTON_RIGHT && pos < strlen(*s)&& strlen(*s) < MAX_PATH)
+				pos++;
+			if (c == BUTTON_DELETE && pos < strlen(*s))
+				deleteSymbol(*s, pos);
+			break;
+		case BUTTON_BACKSPACE:
+			if(pos > 0)
+				deleteSymbol(*s, --pos);
+			break;
+		default:
+			if(pos >= 0 && pos <= strlen(*s) && strlen(*s) < MAX_PATH)
+				insertInStr(*s, pos++, c);
+		}
+		printTextWithCursor(*s, pos);	
+	}
+#ifdef FOR_RUSSIAN
+	setlocale(LC_ALL, "Russian");
+#endif
+	putchar('\n');
+}
